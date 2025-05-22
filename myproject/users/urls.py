3b6696da0530659f50d8a61ebabf766e1c450533
ali-osaid01 <aliusaid55@gmail.com>
2025-05-22 06:50:01 +0500

    🚀 APIs versioning 🚀

--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -1,22 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from . import views
+from .views import RegisterView, LoginView, LogoutView, UserProfileView,UserViewSet
 
 router = DefaultRouter()
-router.register(r'users', views.UserViewSet)
-
+router.register(r'users', UserViewSet)
 
 v1_urlpatterns = [
-   
-    path('auth/register/', views.RegisterView.as_view(), name='register'),
-    path('auth/login/', views.LoginView.as_view(), name='login'),
-    path('auth/logout/', views.LogoutView.as_view(), name='logout'),
-    
-    path('profile/', views.UserProfileView.as_view(), name='profile'),
-    
+    path('auth/register/', RegisterView.as_view(), name='register'),
+    path('auth/login/', LoginView.as_view(), name='login'),
+    path('auth/logout/', LogoutView.as_view(), name='logout'),
+    path('profile/', UserProfileView.as_view(), name='profile'),
     path('', include(router.urls)),
+]
+
+v2_urlpatterns = [
+    path('auth/register/', RegisterView.as_view(), name='register'),
 ]
 
 urlpatterns = [
     path('v1/', include((v1_urlpatterns, 'v1'), namespace='v1')),
+    path('v2/', include((v2_urlpatterns, 'v2'), namespace='v2')),   
 ] 
